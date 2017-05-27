dbe85afc2757b9f3baaaed94ebd85524eaca56fe
zouxu <izouxv@gmail.com>
2017-05-27 13:06:14 +0800

    no message

--- sqlite3/sqlite3-umbrella.h
+++
@@ -1,8 +0,0 @@
-@import Foundation;
-
-#import "sqlite3.h"
-#import "sqlite3ext.h"
-
- double sqlite3VersionNumber;//FOUNDATION_EXPORT
- const unsigned char sqlite3VersionString[];//FOUNDATION_EXPORT
-
